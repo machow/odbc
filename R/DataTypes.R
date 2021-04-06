bb0a6cd2ee3dced3101b02bb1c7c9771cab3f01b
Michael Chow <mc_al_github@fastmail.com>
2021-04-06 18:49:51 -0400

    update snowflake DataTypes to max varchar length

--- R/DataTypes.R
+++ R/DataTypes.R
@@ -300,16 +300,16 @@ odbcDataType.Oracle <- function(con, obj, ...) {
 `odbcDataType.Snowflake` <- function(con, obj, ...) {
   switch_type(
     obj,
-    factor = "VARCHAR(255)",
+    factor = "VARCHAR(16777216)",
     datetime = "TIMESTAMP",
     date = "DATE",
     time = "TIME",
-    binary = "VARBINARY(255)",
+    binary = "VARBINARY(16777216)",
     integer = "INTEGER",
     double = "DOUBLE PRECISION",
-    character = "VARCHAR(255)",
+    character = "VARCHAR(16777216)",
     logical = "BOOLEAN",
-    list = "VARCHAR(255)",
+    list = "VARCHAR(16777216)",
     stop("Unsupported type", call. = FALSE)
   )
 }
